b734ebbfcca2f966377e84d5dec91834a69b9762
Steven1112 <sidiw1112@gmail.com>
2017-02-28 14:13:16 -0800

    Added script to use vive controller laser pointing and triggering the greybox of restart button

---
+++ Assets/Script/InteractiveRestartObject.cs
@@ -0,0 +1,36 @@
+﻿namespace VRTK.Examples
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class InteractiveRestartObject : VRTK_InteractableObject
+    {
+
+        public override void StartUsing(GameObject usingObject)
+        {
+            base.StartUsing(usingObject);
+
+			SceneManager.LoadScene("VFX_28Feb_Restart");
+			Debug.Log ("Restart!");
+            //SceneManager.LoadScene(restartSceneName);
+        }
+
+        public override void StopUsing(GameObject usingObject)
+        {
+            base.StopUsing(usingObject);
+
+			//SceneManager.LoadScene("VFX_28Feb_Restart");
+            //SceneManager.LoadScene(restartSceneName);
+			//Debug.Log ("Restart!");
+        }
+
+        protected void Start()
+        {
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+    }
+}
