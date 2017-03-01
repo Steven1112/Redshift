326c5f3ca69fecff8d293466c020e28c011b3e94
Steven1112 <sidiw1112@gmail.com>
2017-02-28 17:48:19 -0800

    Created five prefabs of the effect that dust coming from different asteroid

---
+++ Assets/Assets/Script/Clump.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clump : MonoBehaviour
+{
+
+    int numCollision;
+    Vector3 velocity;
+    Vector3 scale;
+    GameObject planet;
+    GameObject asteroid;
+    //GameObject sparkle;
+
+    Vector3 planetScale;
+    double planetScaleInc;
+
+    Vector3 asteroidScale;
+    double asteroidScaleDec;
+
+	Vector3 planetCenter;
+
+    public AudioClip colSound;
+    //public AnimationManager animationManager;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (string.Equals(col.gameObject.tag, "protoplanet"))
+        {
+            switch (numCollision)
+            {
+			case 0:
+				SoundManager.instance.playSingle ("effectSource", colSound);
+				Debug.Log ("collision " + numCollision + " happened");
+
+				planet = col.gameObject;
+				planetCenter = planet.transform.position;
+				//planet.GetComponent<Rigidbody> ().angularVelocity = new Vector3 (0, 100, 0);
+
+				asteroid = gameObject;
+				asteroid.GetComponent<SphereCollider> ().radius /= 3.0f;
+				Vector3 velocity = new Vector3 (planetCenter.x - asteroid.transform.position.x, planetCenter.y - asteroid.transform.position.y, planetCenter.z - asteroid.transform.position.z);
+				Debug.Log (velocity);
+				asteroid.GetComponent<Rigidbody>().velocity = velocity;
+
+                    planetScale = planet.transform.localScale;
+                    planetScaleInc = (double)planetScale.x * 0.00002;
+
+                    asteroidScale = asteroid.transform.localScale;
+                    asteroidScaleDec = (double)asteroidScale.x * 0.00004;
+
+				    asteroid.transform.parent = planet.transform;
+				    //PlanetCreator.instance.collected[PlanetCreator.instance.numMaterialCollected] = asteroid;
+
+                    //call addMaterial() and collision parameters to be passed for visual effects
+                    PlanetCreator.instance.addMaterial(gameObject);
+
+                    //AnimationManager.instance.asteroidExplosion.Play();
+
+                    StartCoroutine(Replay());
+
+                    numCollision++;
+
+                    if (PlanetCreator.instance.numMaterialCollected == 3)
+                    {
+                        // destory unused asteroids
+                        GameObject asteroids = UnityEngine.GameObject.FindGameObjectWithTag("asteroids");
+                        Destroy(asteroids);
+                    }
+
+                    break;
+                case 1:
+                    //sparkle = (GameObject)Instantiate(Resources.Load("sparkle"));
+                    //sparkle.transform.position = asteroid.transform.position;
+                    //sparkle.transform.SetParent(asteroid.transform);
+                    Debug.Log("collision " + numCollision + " happened");
+                    asteroid.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+
+                    planet.GetComponent<Rigidbody>().mass += asteroid.GetComponent<Rigidbody>().mass;
+
+				    // AnimationManager.instance.simpleExplosion.Play();
+                    StartCoroutine(Replay());
+                    numCollision++;
+
+				if(PlanetCreator.instance.numMaterialCollected == 3){
+					AnimationManager.instance.playFormationEffect();
+				}
+
+                    break;
+                default:
+                    // do nothing
+                    break;
+            }
+        }
+    }
+
+    void Update()
+    {
+
+        scale = gameObject.transform.localScale;
+        if (numCollision == 2)
+        {
+            if (hasScale(scale))
+            {
+                if (planet != null)
+                {
+                    absorb(planet, asteroid);
+					increaseSpin();
+                }
+            }
+            else
+            {
+                Destroy(asteroid);
+                			
+				if(PlanetCreator.instance.numMaterialCollected == 3){
+
+                    Debug.Log("The third asteroid is completely absorbed");
+                    PlanetCreator.instance.lastAbsorbed();
+					AnimationManager.instance.seeResults.Stop();
+					PlanetCreator.instance.canRestart = true;
+				}
+            }
+        }
+        /*
+        if (sparkle != null) {
+            sparkle.transform.localScale = asteroid.transform.localScale;
+        }
+        */
+    }
+
+    bool hasScale(Vector3 scale)
+    {
+        if (scale.x > 0 && scale.y > 0 && scale.z > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    void absorb(GameObject planet, GameObject asteroid)
+    {
+		//Debug.Log("absorbing...");
+		asteroid.transform.parent = null;
+        planet.transform.localScale += new Vector3((float) planetScaleInc, (float) planetScaleInc, (float) planetScaleInc);
+        asteroid.transform.localScale -= new Vector3((float)asteroidScaleDec, (float)asteroidScaleDec, (float)asteroidScaleDec);
+		asteroid.transform.parent = planet.transform;
+    }
+
+    IEnumerator Replay()
+    {
+        yield return new WaitForSeconds(2);
+        //AnimationManager.instance.asteroidExplosion.Stop();
+        AnimationManager.instance.commonHitExplosion.Stop();
+        // soundManager.hitOn.Stop();
+    }
+
+    void increaseSpin() {
+		planet.GetComponent<Spin>().speed += 0.007f;
+	}
+}
+
---
+++ Assets/Assets/Script/Formation.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Formation : MonoBehaviour {
+    public string name;
+    public HashSet<string> ingredients;
+    AnimationClip animation;
+    public GameObject finalStage;
+
+    public Formation(string name, HashSet<string> ingredients, GameObject planet) {
+        this.ingredients = new HashSet<string>();
+        this.name = name;
+        this.ingredients = ingredients;
+        this.finalStage = planet;
+    }
+
+    public HashSet<string> getIngredients() {
+        return ingredients;
+    }
+
+    public void form(GameObject protoPlanet) {
+		//wait for a few seconds
+
+		// add formed planet to collection
+		PlanetCollection.instance.addPlanetToCollection(this);
+
+		//display animation
+
+		//replace finalStage with the current protoplanet object
+		GameObject formed = (GameObject)Instantiate (finalStage);
+		formed.AddComponent<Spin> ();
+		formed.GetComponent<Spin> ().speed = protoPlanet.GetComponent<Spin> ().speed;
+		formed.GetComponent<Spin> ().vector = protoPlanet.GetComponent<Spin> ().vector;
+		Debug.Log (finalStage.name + " is created");
+		formed.transform.position = protoPlanet.transform.position;
+		formed.transform.rotation = protoPlanet.transform.rotation;
+		//formed.transform.localScale = protoPlanet.transform.localScale;
+
+        if (protoPlanet.transform.parent != null)
+        {
+            Destroy(protoPlanet.transform.parent.gameObject);
+			Debug.Log ("protoplanet has parent");
+        }
+        else {}
+
+		AnimationManager.instance.seeResults.Stop();
+        Destroy(protoPlanet);
+    
+
+        // destory asteroids
+        GameObject collected = UnityEngine.GameObject.FindGameObjectWithTag("collected");
+        Destroy(collected);
+    }
+
+    public void setIngredients(HashSet<string> ingredients) {
+        this.ingredients = ingredients;
+    }
+    public void setName(string name) {
+        this.name = name;
+    }
+		
+}
