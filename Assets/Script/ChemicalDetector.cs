6032b2db93216941df70d142753794c554371919
Robyn Choi <robyn3choi@gmail.com>
2017-03-02 16:22:59 -0800

    added showing asteroid info

--- Assets/Script/ChemicalDetector.cs
+++ Assets/Script/ChemicalDetector.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChemicalDetector : MonoBehaviour {
     public GameObject infoObject;
     GameObject focusedObject = null;
+
+	// the thickness of the raycast
+	//float thickness = 10.0f;
 
     // Use this for initialization
     void Start () {
@@ -13,18 +17,20 @@ public class ChemicalDetector : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
-
+        Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
         RaycastHit hit;
-        if (Physics.Raycast(transform.position, transform.forward, out hit) && hit.transform.parent.tag == "asteroids")
+		// Physics.SphereCast(transform.position,thickness,transform.forward, out hit)
+		if (Physics.Raycast(transform.position,transform.forward, out hit) && hit.transform.parent != null)
         {
-            // is looking at an asteroid
-            if (hit.transform != null)
-            {
-                focusedObject = hit.transform.gameObject;
-                ShowInfo(hit.transform);
-            }
-            Debug.Log(hit.transform.name);
+			if (hit.transform.parent.tag == "asteroids") {
+				// is looking at an asteroid
+				if (hit.transform != null)
+				{
+					focusedObject = hit.transform.gameObject;
+					ShowInfo(hit.transform);
+				}
+				Debug.Log(hit.transform.name);
+			}
         }
         else
         {
@@ -43,9 +49,12 @@ public class ChemicalDetector : MonoBehaviour {
     {
         infoObject.SetActive(true);
         infoObject.transform.parent = hit;
+		infoObject.transform.rotation = Quaternion.identity;
         Vector3 temp = Vector3.zero;
-        float radius = hit.gameObject.GetComponent<SphereCollider>().radius * hit.transform.localScale.x;
-        temp.y = radius + 1;
+        float radius = hit.gameObject.GetComponent<SphereCollider>().radius;
+        temp.y = radius + 0.005f;
         infoObject.transform.localPosition = temp;
+		infoObject.transform.GetChild (0).GetComponent<Text> ().text = "contains " + hit.transform.tag; 
+		Debug.Log ("showed info");
     }
 }
