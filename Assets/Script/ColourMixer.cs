65cca9bf2ca095231a33823b760b66122330ac4a
muse83831 <muse83831@hotmail.com>
2017-03-14 16:27:26 -0700

    implemented more protoplanet feedback with colour change

--- Assets/Script/ColourMixer.cs
+++ Assets/Script/ColourMixer.cs
@@ -22,12 +22,19 @@ public class ColourMixer : MonoBehaviour {
         for(int i = 0; i < materialsToBeMixed.Length; i++)
         {
             Material material = materialsToBeMixed[i];
-            if(material.name == "face" || material.name == "subface color")
+
+			if(material.name == "face (Instance)" || material.name == "subface color (Instance)")
             {
-                Color mixedColour = Color.Lerp(material.color, colouring, 0.5f);
+                Color mixedColour = Color.Lerp(material.color, colouring, 0.7f);
                 material.color = mixedColour;
             }
-        }
+			/*
+			Debug.Log ("material name is:" + material.name);
+			Debug.Log ("colouring is:" + colouring);
+			Debug.Log ("material to be coloured: " + material.color);
+			Color mixedColour = Color.Lerp(material.color, colouring, 0.6f);
+			material.color = mixedColour;*/
+		}
     }
 
 }
