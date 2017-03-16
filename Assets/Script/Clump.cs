ebdb4ef7ccfed5085709cdb211123ca0ac46524f
muse83831 <muse83831@hotmail.com>
2017-03-16 12:22:44 -0700

    merged with colour

--- Assets/Script/Clump.cs
+++ Assets/Script/Clump.cs
@@ -4,160 +4,137 @@ using UnityEngine;
 
 public class Clump : MonoBehaviour
 {
+	ColourMixer colourMixer;
+	int numCollision;
+	Vector3 velocity;
+	Vector3 scale;
+	GameObject planet;
+	GameObject asteroid;
+	//GameObject sparkle;
 
-    int numCollision;
-    Vector3 velocity;
-    Vector3 scale;
-    GameObject planet;
-    GameObject asteroid;
-    //GameObject sparkle;
+	Vector3 planetScale;
+	double planetScaleInc;
 
-    Vector3 planetScale;
-    double planetScaleInc;
-
-    Vector3 asteroidScale;
-    double asteroidScaleDec;
+	Vector3 asteroidScale;
+	double asteroidScaleDec;
 
 	Vector3 planetCenter;
 
-    public AudioClip colSound;
-    //public AnimationManager animationManager;
+	public AudioClip colSound;
+	//public AnimationManager animationManager;
 
-    void OnCollisionEnter(Collision col)
-    {
-        if (string.Equals(col.gameObject.tag, "protoplanet"))
-        {
-            switch (numCollision)
-            {
+	void Start()
+	{
+		colourMixer = UnityEngine.GameObject.FindGameObjectWithTag("colourMixer").GetComponent<ColourMixer>();
+	}
+
+	void OnCollisionEnter(Collision col)
+	{
+		if (string.Equals(col.gameObject.tag, "protoplanet"))
+		{
+			switch (numCollision)
+			{
 			case 0:
 				SoundManager.instance.playSingle ("effectSource", colSound);
 				Debug.Log ("collision " + numCollision + " happened");
 
 				planet = col.gameObject;
 				planetCenter = planet.transform.position;
-				//planet.GetComponent<Rigidbody> ().angularVelocity = new Vector3 (0, 100, 0);
 
 				asteroid = gameObject;
 				asteroid.GetComponent<SphereCollider> ().radius /= 3.0f;
 				Vector3 velocity = new Vector3 (planetCenter.x - asteroid.transform.position.x, planetCenter.y - asteroid.transform.position.y, planetCenter.z - asteroid.transform.position.z);
-				Debug.Log (velocity);
 				asteroid.GetComponent<Rigidbody>().velocity = velocity;
 
-                    planetScale = planet.transform.localScale;
-                    planetScaleInc = (double)planetScale.x * 0.00002;
+				planetScale = planet.transform.localScale;
+				planetScaleInc = (double)planetScale.x * 0.00002;
 
-                    asteroidScale = asteroid.transform.localScale;
-                    asteroidScaleDec = (double)asteroidScale.x * 0.00004;
+				asteroidScale = asteroid.transform.localScale;
+				asteroidScaleDec = (double)asteroidScale.x * 0.00004;
 
-				    asteroid.transform.parent = planet.transform;
-				    //PlanetCreator.instance.collected[PlanetCreator.instance.numMaterialCollected] = asteroid;
+				asteroid.transform.parent = planet.transform;
 
-                    //call addMaterial() and collision parameters to be passed for visual effects
-                    PlanetCreator.instance.addMaterial(gameObject);
+				PlanetCreator.instance.addMaterial(gameObject);
+				colourMixer.MixColour(planet,asteroid);
 
-                    //AnimationManager.instance.asteroidExplosion.Play();
+				StartCoroutine(Replay());
 
-                    StartCoroutine(Replay());
+				numCollision++;
 
-                    numCollision++;
+				if (PlanetCreator.instance.numMaterialCollected == 3)
+				{
+					// destory unused asteroids
+					GameObject asteroids = UnityEngine.GameObject.FindGameObjectWithTag("asteroids");
+					Destroy(asteroids);
+				}
 
-                    if (PlanetCreator.instance.numMaterialCollected == 3)
-                    {
-                        // destory unused asteroids
-                        GameObject asteroids = UnityEngine.GameObject.FindGameObjectWithTag("asteroids");
-                        Destroy(asteroids);
-                    }
+				break;
+			case 1:
+				Debug.Log("collision " + numCollision + " happened");
+				asteroid.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
-                    break;
-                case 1:
-                    //sparkle = (GameObject)Instantiate(Resources.Load("sparkle"));
-                    //sparkle.transform.position = asteroid.transform.position;
-                    //sparkle.transform.SetParent(asteroid.transform);
-                    Debug.Log("collision " + numCollision + " happened");
-                    asteroid.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+				planet.GetComponent<Rigidbody>().mass += asteroid.GetComponent<Rigidbody>().mass;
 
-                    planet.GetComponent<Rigidbody>().mass += asteroid.GetComponent<Rigidbody>().mass;
+				StartCoroutine(Replay());
+				numCollision++;
+				break;
+			default:
+				// do nothing
+				break;
+			}
+		}
+	}
 
-				    // AnimationManager.instance.simpleExplosion.Play();
-                    StartCoroutine(Replay());
-                    numCollision++;
-				/*
-				if(PlanetCreator.instance.numMaterialCollected == 3){
-					AnimationManager.instance.playFormationEffect();
-				}*/
+	void Update()
+	{
 
-                    break;
-                default:
-                    // do nothing
-                    break;
-            }
-        }
-    }
-
-    void Update()
-    {
-
-        scale = gameObject.transform.localScale;
-        if (numCollision == 2)
-        {
-            if (hasScale(scale))
-            {
-                if (planet != null)
-                {
-                    absorb(planet, asteroid);
+		scale = gameObject.transform.localScale;
+		if (numCollision == 2)
+		{
+			if (hasScale(scale))
+			{
+				if (planet != null)
+				{
+					absorb(planet, asteroid);
 					increaseSpin();
-                }
-            }
-            else
-            {
-                Destroy(asteroid);
-                			
+				}
+			}
+			else
+			{
+				Destroy(asteroid);
+
 				if(PlanetCreator.instance.numMaterialCollected == 3){
 
-                    Debug.Log("The third asteroid is completely absorbed");
-                    PlanetCreator.instance.lastAbsorbed();
+					Debug.Log("The third asteroid is completely absorbed");
+					PlanetCreator.instance.lastAbsorbed();
 					AnimationManager.instance.seeResults.Stop();
 					PlanetCreator.instance.canRestart = true;
 				}
-            }
-        }
-        /*
-        if (sparkle != null) {
-            sparkle.transform.localScale = asteroid.transform.localScale;
-        }
-        */
-    }
+			}
+		}
+	}
 
-    bool hasScale(Vector3 scale)
-    {
-        if (scale.x > 0 && scale.y > 0 && scale.z > 0)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
-    }
+	bool hasScale(Vector3 scale)
+	{
+		return (scale.x > 0 && scale.y > 0 && scale.z > 0);
+	}
 
-    void absorb(GameObject planet, GameObject asteroid)
-    {
+	void absorb(GameObject planet, GameObject asteroid)
+	{
 		//Debug.Log("absorbing...");
 		asteroid.transform.parent = null;
-        planet.transform.localScale += new Vector3((float) planetScaleInc, (float) planetScaleInc, (float) planetScaleInc);
-        asteroid.transform.localScale -= new Vector3((float)asteroidScaleDec, (float)asteroidScaleDec, (float)asteroidScaleDec);
+		planet.transform.localScale += new Vector3((float) planetScaleInc, (float) planetScaleInc, (float) planetScaleInc);
+		asteroid.transform.localScale -= new Vector3((float)asteroidScaleDec, (float)asteroidScaleDec, (float)asteroidScaleDec);
 		asteroid.transform.parent = planet.transform;
-    }
+	}
 
-    IEnumerator Replay()
-    {
-        yield return new WaitForSeconds(2);
-        //AnimationManager.instance.asteroidExplosion.Stop();
-        AnimationManager.instance.commonHitExplosion.Stop();
-        // soundManager.hitOn.Stop();
-    }
+	IEnumerator Replay()
+	{
+		yield return new WaitForSeconds(2);
+		AnimationManager.instance.commonHitExplosion.Stop();
+	}
 
-    void increaseSpin() {
+	void increaseSpin() {
 		planet.GetComponent<Spin>().speed += 0.007f;
 	}
 }
---
+++ Assets/Script/ColourMixer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourMixer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MixColour(GameObject colouredObject, GameObject colouringObject) {
+
+        // get the materials of the colouredObject and the colour of the colouringObject
+        Material[] materialsToBeMixed = UnityEngine.GameObject.FindGameObjectWithTag("protoplanet").GetComponent<Renderer>().materials;
+        Color colouring = colouringObject.GetComponent<Renderer>().material.color;
+
+		for (int i = 0; i < materialsToBeMixed.Length; i++) {
+			Material material = materialsToBeMixed [i];
+
+			if (material.name == "face (Instance)" || material.name == "subface color (Instance)") {
+				Color mixedColour = Color.Lerp (material.color, colouring, 0.6f);
+				material.color = mixedColour;
+			}
+		}
+    }
+
+}
