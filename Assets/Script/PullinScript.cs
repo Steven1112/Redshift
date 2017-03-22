5f4cb9c99d841133fb6792e02993b60230658a01
Steven1112 <sidiw1112@gmail.com>
2017-03-22 15:34:01 -0700

    Fixing the animation in process

---
+++ Assets/Script/PullinScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullinScript : MonoBehaviour {
+
+	[Header("Animations")]
+	public Animator asteroidPullin;
+
+	public static PullinScript instance = null;
+	// Use this for initialization
+	void Start () {
+		// create instance
+		if (instance == null)
+		{
+			instance = this;
+		}
+		else if (instance != this)
+		{
+			Destroy(gameObject);
+		}
+		//DontDestroyOnLoad(gameObject);
+
+		asteroidPullin = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		//hot key for testing
+		if (Input.GetKeyDown(KeyCode.T))
+		{
+			asteroidPullin.Play("AsteroidPullin");
+			// hot key for testing sound
+			//SoundManager.instance.explosionOxygen.Play();
+			//SoundManager.instance.playSingle("explosionOxygen", colOxygenSound);
+		}
+
+	}
+
+	public void playAsteroidPullinEffect()
+	{
+		asteroidPullin.Play("AsteroidPullin");
+	}
+
+	public void stopAsteroidPullinEffect()
+	{
+		asteroidPullin.Stop();
+	}
+}
